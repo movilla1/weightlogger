45c0a9000c1a55e00e957a39d8982102fe790894
Mario O. Villarroel <movilla@gmail.com>
2018-09-05 10:40:01 -0300

    Login working and sb-admin-2 template on the way

--- elcan_weight/app/controllers/application_controller.rb
+++ elcan_weight/app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
---
+++ elcan_weight/app/models/usuario.rb
@@ -0,0 +1,8 @@
+class Usuario < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable, 
+         :trackable, :validatable
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+end
