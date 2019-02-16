18d64b348a06b815a9dd4e5456bf2a93bf36d2a1
Mario O. Villarroel <movilla@gmail.com>
2019-02-16 11:36:39 -0300

    User Manual start, new scale board  ( jumper selects  between scales)

--- weightlogger_scale/weightlogger_scale.ino
+++ weightlogger_scale/weightlogger_scale.ino
@@ -1,29 +1,34 @@
 #include <Wire.h>
 
-#define OLDSCALE 1
-//#define NEWSCALE 1
 #define SCALE_I2C_ADDRESS 0x42
+#define OLD_SCALE = 29
+#define GAMMA_SCALE = 60
 #define MAX_WEIGHT_SIZE 7
 #define READY 0
 #define SEND_INIT_STR 16
 #define SEND_WEIGHT 64
 #define LED 8
+#define SETUP_JMP 5
 #define WEIGHT_LENGTH 7
 
 char weight[10];
 char wireBuffer[10];
 char pos;
+byte scaleMode;
 byte sysState;
 
 void setup() {
-#ifdef NEWSCALE
-  Serial.begin(9600); // serial standard for scales
-#endif
-#ifdef OLDSCALE
-  Serial.begin(4800);
-#endif
   Wire.begin(SCALE_I2C_ADDRESS); //begin i2c slave
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(SETUP_JMP, INPUT_PULLUP);
+  delay(300); // wait for the input to become stable
+  if (digitalRead(SETUP_JMP) == 1) {
+    scaleMode = OLD_SCALE;
+    Serial.begin(4800);
+  } else {
+    scaleMode = GAMMA_SCALE;
+    Serial.begin(9600); // serial standard for scales
+  }
   Wire.onRequest(answerMaster);
   Wire.onReceive(checkCommand);
   memset(weight, 0, sizeof(weight));
@@ -36,30 +41,14 @@ void loop() {
   byte tmp;
   switch(sysState) {
     case READY:
-#ifdef NEWSCALE
-      pos = Serial.readBytesUntil('\n', weight, sizeof(weight));
-      weight[pos+1] = 0x00;
-      memcpy(wireBuffer, weight, sizeof(wireBuffer));
-      memset(weight,0, sizeof(weight));
-#endif
-#ifdef OLDSCALE
-      if (Serial.available() >= 7) {
-        for (pos=0; pos < 7; pos++) {
-          tmp = Serial.read();
-          if (tmp == 0x1C || tmp == 0x1B) {
-            memcpy(wireBuffer, weight, WEIGHT_LENGTH);
-            memset(weight, 0, sizeof(weight));
-          } else {
-            weight[pos] = tmp;
-          }
-          if (tmp == 0x1C) {
-            digitalWrite(LED, HIGH);
-          } else {
-            digitalWrite(LED, LOW);
-          }
-        }
+      if (scaleMode == GAMMA_SCALE) {
+        pos = Serial.readBytesUntil('\n', weight, sizeof(weight));
+        weight[pos+1] = 0x00;
+        memcpy(wireBuffer, weight, sizeof(wireBuffer));
+        memset(weight,0, sizeof(weight));
+      } else {
+        read_old_scale();
       }
-#endif
       break;
   }
 }
@@ -99,5 +88,25 @@ void answerMaster() {
 void sendLastReadedWeight() {
   for (char i=0; i < MAX_WEIGHT_SIZE; i++) {
     Wire.write(wireBuffer[i]);
+  }
+}
+
+void read_old_scale() {
+  byte tmp;
+  if (Serial.available() >= 7) {
+    for (pos=0; pos < 7; pos++) {
+      tmp = Serial.read();
+      if (tmp == 0x1C || tmp == 0x1B) {
+        memcpy(wireBuffer, weight, WEIGHT_LENGTH);
+        memset(weight, 0, sizeof(weight));
+      } else {
+        weight[pos] = tmp;
+      }
+      if (tmp == 0x1C) {
+        digitalWrite(LED, HIGH);
+      } else {
+        digitalWrite(LED, LOW);
+      }
+    }
   }
 }
